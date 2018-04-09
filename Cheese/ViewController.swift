678dc4a869fcab1ddd9e46ed1d0dba59acafe171
黃暉德 <WadeHuang@wadehuang-mba.local>
2018-04-09 22:28:27 +0800

    Initial Commit

---
+++ Cheese/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Cheese
+//
+//  Created by 黃暉德 on 2018/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
